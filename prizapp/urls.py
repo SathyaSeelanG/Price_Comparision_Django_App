0a7b718f9e60f43042ddcc2201f7f0291efbd459
Sathya Seelan <120622131+SathyaSeelanG@users.noreply.github.com>
2023-06-04 09:04:46 +0530

    Update urls.py

--- prizapp/urls.py
+++ prizapp/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import include, path
-from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -24,4 +24,3 @@ urlpatterns = [
 ]
 
 
-urlpatterns += staticfiles_urlpatterns
